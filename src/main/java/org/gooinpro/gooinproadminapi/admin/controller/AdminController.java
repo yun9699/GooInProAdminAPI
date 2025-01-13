3d4a7ddc6e00b37358fb605b3ef9e7c4fa39bacb
yun9699 <cha23215@daum.net>
2025-01-13 16:49:59 +0900

    chore: security package add

---
+++ src/main/java/org/gooinpro/gooinproadminapi/admin/controller/AdminController.java
@@ -0,0 +1,13 @@
+package org.gooinpro.gooinproadminapi.admin.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/admin/api/v1/admin")
+@Log4j2
+@RequiredArgsConstructor
+public class AdminController {
+}
