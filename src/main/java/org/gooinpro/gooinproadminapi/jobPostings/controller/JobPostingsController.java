f3864e6a2f4422aac1fc75306b0a1aef58183f97
jhyeong <hyeongcode@gmail.com>
2025-01-15 15:44:44 +0900

    feat: jobPostings allList fn add

--- src/main/java/org/gooinpro/gooinproadminapi/jobPostings/controller/JobPostingsController.java
+++ src/main/java/org/gooinpro/gooinproadminapi/jobPostings/controller/JobPostingsController.java
@@ -2,7 +2,12 @@ package org.gooinpro.gooinproadminapi.jobPostings.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.gooinpro.gooinproadminapi.common.dto.PageRequestDTO;
+import org.gooinpro.gooinproadminapi.common.dto.PageResponseDTO;
+import org.gooinpro.gooinproadminapi.jobPostings.dto.JobPostingsListDTO;
 import org.gooinpro.gooinproadminapi.jobPostings.service.JobPostingsService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,4 +18,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class JobPostingsController {
 
     private final JobPostingsService jobPostingsService;
+
+    //전체 구인 공고 리스트 get
+    @GetMapping("allList")
+    public ResponseEntity<PageResponseDTO<JobPostingsListDTO>> allListController(PageRequestDTO pageRequestDTO) {
+
+        log.info("JobPostingsController: allList");
+        return ResponseEntity.ok(jobPostingsService.jobPostingsAllListService(pageRequestDTO));
+    }
 }
--- src/main/java/org/gooinpro/gooinproadminapi/jobPostings/service/JobPostingsService.java
+++ src/main/java/org/gooinpro/gooinproadminapi/jobPostings/service/JobPostingsService.java
@@ -2,6 +2,9 @@ package org.gooinpro.gooinproadminapi.jobPostings.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.gooinpro.gooinproadminapi.common.dto.PageRequestDTO;
+import org.gooinpro.gooinproadminapi.common.dto.PageResponseDTO;
+import org.gooinpro.gooinproadminapi.jobPostings.dto.JobPostingsListDTO;
 import org.gooinpro.gooinproadminapi.jobPostings.repository.JobPostingsRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,4 +16,11 @@ import org.springframework.transaction.annotation.Transactional;
 public class JobPostingsService {
 
     private final JobPostingsRepository jobPostingsRepository;
+
+    //전체 구인 공고 리스트 get
+    public PageResponseDTO<JobPostingsListDTO> jobPostingsAllListService(PageRequestDTO pageRequestDTO) {
+
+        log.info("JobPostingsService: getAllList");
+        return jobPostingsRepository.jobPostingsList(pageRequestDTO);
+    }
 }
