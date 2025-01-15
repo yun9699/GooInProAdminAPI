635806a8c316c59dd1f087a2aa6c1774f399c3d3
dasol <ekthf988172@gmail.com>
2025-01-15 22:16:14 +0900

    feat: PartTimer fn add

--- src/main/java/org/gooinpro/gooinproadminapi/parttimer/repository/search/PartTimerSearch.java
+++ src/main/java/org/gooinpro/gooinproadminapi/parttimer/repository/search/PartTimerSearch.java
@@ -2,10 +2,13 @@ package org.gooinpro.gooinproadminapi.parttimer.repository.search;
 
 import org.gooinpro.gooinproadminapi.common.dto.PageRequestDTO;
 import org.gooinpro.gooinproadminapi.common.dto.PageResponseDTO;
-import org.gooinpro.gooinproadminapi.parttimer.dto.PartTimerDTO;
+import org.gooinpro.gooinproadminapi.parttimer.dto.PartTimerDetailDTO;
+import org.gooinpro.gooinproadminapi.parttimer.dto.PartTimerListDTO;
 
 public interface PartTimerSearch {
 
-    PageResponseDTO<PartTimerDTO> getPartTimerList(PageRequestDTO pageRequestDTO);
+    PageResponseDTO<PartTimerListDTO> getPartTimerList(PageRequestDTO pageRequestDTO);
+
+    PartTimerDetailDTO partTimerDetail(Long pno);
 
 }
--- src/main/java/org/gooinpro/gooinproadminapi/parttimer/service/PartTimerService.java
+++ src/main/java/org/gooinpro/gooinproadminapi/parttimer/service/PartTimerService.java
@@ -5,7 +5,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.gooinpro.gooinproadminapi.common.dto.PageRequestDTO;
 import org.gooinpro.gooinproadminapi.common.dto.PageResponseDTO;
-import org.gooinpro.gooinproadminapi.parttimer.dto.PartTimerDTO;
+import org.gooinpro.gooinproadminapi.parttimer.dto.PartTimerDetailDTO;
+import org.gooinpro.gooinproadminapi.parttimer.dto.PartTimerListDTO;
 import org.gooinpro.gooinproadminapi.parttimer.repository.PartTimerRepository;
 import org.springframework.stereotype.Service;
 
@@ -17,8 +18,12 @@ public class PartTimerService {
 
     private final PartTimerRepository partTimerRepository;
 
-    public PageResponseDTO<PartTimerDTO> partTimerList(PageRequestDTO pageRequestDTO) {
+    public PageResponseDTO<PartTimerListDTO> partTimerList(PageRequestDTO pageRequestDTO) {
 
         return partTimerRepository.getPartTimerList(pageRequestDTO);
+    }
+
+    public PartTimerDetailDTO partTimerDetail(Long pno) {
+        return partTimerRepository.partTimerDetail(pno);
     }
 }
