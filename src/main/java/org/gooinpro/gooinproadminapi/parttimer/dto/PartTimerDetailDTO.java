bc49e6fbbaa2cc99dfc0a88c785f6a358d9fbccc
dasol <ekthf988172@gmail.com>
2025-01-16 15:56:16 +0900

    feat: PartTimer image fn add

--- src/main/java/org/gooinpro/gooinproadminapi/parttimer/dto/PartTimerDetailDTO.java
+++ src/main/java/org/gooinpro/gooinproadminapi/parttimer/dto/PartTimerDetailDTO.java
@@ -15,6 +15,10 @@ public class PartTimerDetailDTO {
 
     private String pname; // 근로자 이름
 
+    private String pdifilename; // 근로자 서류 사진
+
+    private String pifilename; // 근로자 프로필 사진
+
     private Timestamp pbirth; // 생년월일
 
     private boolean pgender; // 성별 true = 남 , false = 여
